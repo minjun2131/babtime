564c9c4a1fb58a68d89eaedc97f5825248b6cd98
yangsunghun <163554102+yangsunghun@users.noreply.github.com>
2024-11-19 15:02:20 +0900

    refactor: Post 부분 컴포넌트로 분리

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -29,16 +29,18 @@ const Header = ({ searchTerm, setSearchTerm }) => {
     checkUser();
   }, []);
 
+  const isSearchVisible = ['/Main', '/main', '/'].includes(location.pathname);
+
   return (
     <HeaderWrap>
       <Inner>
         <h1>
-          <Link to={`/Main`}>
+          <Link to={`/`}>
             <img src="../images/logo_horizontal.svg" alt="밥타임" />
           </Link>
         </h1>
-        {location.pathname === '/Main' && ( // 경로가 /main일 때만 Search 컴포넌트 렌더링
-          <Search placeholderText="제목으로 검색" value={searchTerm} onChange={handleSearchChange} />
+        {isSearchVisible && (
+          <Search placeholderText="제목으로 검색" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
         )}
 
         <HeaderRight hasUserInfo={hasUserInfo} />
