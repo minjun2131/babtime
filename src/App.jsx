3fe2b7d611b6d7ff047739be183a1fdd2dc9c27f
Eunyeong Park <eunyeong.cloud@gmail.com>
2024-11-20 14:47:22 +0900

    🐛 bugfix: 이미지 수정 후 반영 부분 수정

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
     <>
       <GlobalStyle />
       <Router />
-      <Toast position="top-center" autoClose={100000} closeButton={false} hideProgressBar />
+      <Toast position="top-center" autoClose={2000} closeButton={false} hideProgressBar />
     </>
   );
 };
